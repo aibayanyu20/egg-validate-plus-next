936ad7827ef49a9a86a0606dbf1af8ab70616951
zhuzhengjian <aibyanyu@qq.com>
2021-07-02 09:38:46 +0800

    升级验证器版本，增加ts代码提示工具

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import { Rules } from "async-validator"
 declare module 'egg'{
     interface Context{
         validate(rules:string,query:object):any;
-        validate(rules:object,query:object,scene:string);
-        validate(rules:Rules,query:object,scene:string);
+        validate(rules:object,query:object,scene:string):any;
+        validate(rules:Rules,query:object,scene:string):any;
     }
 }
